7872d210da6e491742d9e66632c4b78b4004a57e
jerrydevelop <hs@192.168.1.68>
2024-06-16 10:48:50 -0600

    feat: add nested routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,5 @@
 import { lazy, LazyExoticComponent } from "react";
-// import { LazyPage1, LazyPage2, LazyPage3 } from "../01-layload/pages";
+import NoLazy from "../01-layload/pages/NoLazy";
 
 // ----------------------------------------------------------------------
 
@@ -12,39 +12,21 @@ interface Route {
   name: string;
 }
 
-// ! webpackChunkName is used to name the chunk file in webpack, on vite doesn't work
-const Lazy1 = lazy(
-  () =>
-    import(/* webpackChunkName: "LazyPage11" */ "../01-layload/pages/LazyPage1")
-);
-const Lazy2 = lazy(
-  () =>
-    import(/* webpackChunkName: "LazyPage22" */ "../01-layload/pages/LazyPage2")
-);
-const Lazy3 = lazy(
-  () =>
-    import(/* webpackChunkName: "LazyPage33" */ "../01-layload/pages/LazyPage3")
-);
+const LazyLayout = lazy(() => import("../01-layload/layout/LazyLayout"));
 
 // ----------------------------------------------------------------------
 
 export const routes: Route[] = [
   {
-    to: "/lazy1",
-    path: "lazy1",
-    Component: Lazy1,
-    name: "Lazy 1",
+    path: "/lazyload/*",
+    to: "/lazyload/",
+    Component: LazyLayout,
+    name: "Lazy Layout",
   },
   {
-    to: "/lazy2",
-    path: "lazy2",
-    Component: Lazy2,
-    name: "Lazy 2",
-  },
-  {
-    to: "/lazy3",
-    path: "lazy3",
-    Component: Lazy3,
-    name: "Lazy 3",
+    to: "/no-lazy",
+    path: "no-lazy",
+    Component: NoLazy,
+    name: "No Lazy 2",
   },
 ];
