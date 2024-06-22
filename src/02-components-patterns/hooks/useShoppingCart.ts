cf4562dc07c6fb65214cb4e2be438dee19366db1
jerrydevelop <hs@192.168.1.68>
2024-06-21 18:11:14 -0600

    feat: add control props alt

--- src/02-components-patterns/hooks/useShoppingCart.ts
+++ src/02-components-patterns/hooks/useShoppingCart.ts
@@ -17,36 +17,21 @@ export const useShoppingCart = () => {
     product: Product;
   }) => {
     // console.log("onProductCountChange", count, product);
+    console.log({ count });
 
     setShoppingCart((oldShoppingCart) => {
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
-
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart,
-        };
+      if (count === 0) {
+        const { [product.id]: _, ...rest } = oldShoppingCart;
+        return rest;
       }
 
-      const { [product.id]: _, ...rest } = oldShoppingCart;
-      return rest;
-
-      // if (count === 0) {
-      //   const { [product.id]: _, ...rest } = oldShoppingCart;
-      //   return rest;
-      // }
-
-      // return {
-      //   ...oldShoppingCart,
-      //   [product.id]: {
-      //     ...product,
-      //     count,
-      //   },
-      // };
+      return {
+        ...oldShoppingCart,
+        [product.id]: {
+          ...product,
+          count,
+        },
+      };
     });
   };
 
